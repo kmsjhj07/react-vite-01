6a8a1e3205abc6627cbb785eeaf9bc7c5d0a3691
BYJ <kmsjhj07@gmail.com>
2025-04-21 13:16:02 +0900

    컴포넌트 생성 실습

---
+++ src/component.tsx
@@ -0,0 +1,11 @@
+const Component = () => {
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <p>리액트는 UI를 만드는 라이브러리입니다.</p>
+        </div>
+    );
+}
+
+export default Component;
+
